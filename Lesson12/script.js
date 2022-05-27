a4bd5b69acb85c4849c8cdbea7ef2019bd44d7d1
Aleksey Akimov <akimov.frontend@gmail.com>
2022-05-27 12:51:24 +0300

    add new foled

---
+++ Lesson12/script.js
@@ -0,0 +1,9 @@
+let numberOfFilms = confirm('Сколько фильмов уже посмотрели?', '');
+
+personalMovieDb = {
+   count: numberOfFilms,
+   movies: '',
+   actors: '',
+   genres: [],
+   privat: false,
+}
